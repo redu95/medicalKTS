aa84f71eb199d109bd8de536b58c246a2ac89051
redu95 <redietmuluken95@gmail.com>
2024-08-14 12:14:43 +0300

    Medic push

--- medicalKTS/src/main/java/com/bannershallmark/controller/MedicInstituteController.java
+++ medicalKTS/src/main/java/com/bannershallmark/controller/MedicInstituteController.java
@@ -102,6 +102,8 @@ public class MedicInstituteController {
 	
 	@GetMapping("/addMedicItems")
 	public String addMedicItems(Model model) throws Exception {
+		MedicItem item = new MedicItem();
+		model.addAttribute("item", item);
 		return "medicInstitute/addItems";
 	}
 	
